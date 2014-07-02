70810661659a9abcc09041c4368742013f936546
vm <vm@vm-virtual-machine.(none)>
2014-07-02 20:05:03 +0800

    140702 with gtk

--- send_filecmd.c
+++ send_filecmd.c
@@ -1,7 +1,7 @@
-/*a.out filename vmname*/
+#include<gtk/gtk.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
@@ -10,130 +10,204 @@
 #include <sys/ioctl.h>
 #include <linux/types.h>
 #include <linux/netlink.h>
+#include <arpa/inet.h>
 #include <errno.h>
 
 #define UEVENT_BUFFER_SIZE       2048
-//int cal_num(char *str)
-//{
-//    int sum = 0;
-//    char *end = str+strlen(str);
-//    char *start = str;
-//    while(start<=end)
-//        sum+=*start++;
-//    return sum%8090+(65535-8090);
-//}
+
+GtkWidget* window;
 
 static int init_hotplug_sock ( void )
 {
-	struct sockaddr_nl snl ;
-	const int buffersize = 16 * 1024 * 1024;
-	int retval ;
+    struct sockaddr_nl snl ;
+    const int buffersize = 16 * 1024 * 1024;
+    int retval ;
 
-	memset (& snl , 0x00, sizeof ( struct sockaddr_nl));
-	snl .nl_family = AF_NETLINK;
-	snl .nl_pid = getpid ();
-	snl .nl_groups = 1;
+    memset (& snl , 0x00, sizeof ( struct sockaddr_nl));
+    snl .nl_family = AF_NETLINK;
+    snl .nl_pid = getpid ();
+    snl .nl_groups = 1;
 
-	int hotplug_sock = socket (PF_NETLINK, SOCK_DGRAM , NETLINK_KOBJECT_UEVENT);
-	if ( hotplug_sock == -1) {
-		printf ( "error getting socket: %s" , strerror ( errno ));
-		return -1;
-	}
+    int hotplug_sock = socket (PF_NETLINK, SOCK_DGRAM , NETLINK_KOBJECT_UEVENT);
+    if ( hotplug_sock == -1) {
+        printf ( "error getting socket: %s" , strerror ( errno ));
+        return -1;
+    }
 
-	/* set receive buffersize */
-	setsockopt ( hotplug_sock , SOL_SOCKET , SO_RCVBUFFORCE, & buffersize , sizeof ( buffersize ));
+    /* set receive buffersize */
+    setsockopt ( hotplug_sock , SOL_SOCKET , SO_RCVBUFFORCE, & buffersize , sizeof ( buffersize ));
 
-	retval = bind ( hotplug_sock , ( struct sockaddr *) & snl , sizeof ( struct sockaddr_nl));
-	if (retval < 0) {
-		printf ( "bind failed: %s" , strerror ( errno ));
-		close ( hotplug_sock );
-		hotplug_sock = -1;
-		return -1;
-	}
+    retval = bind ( hotplug_sock , ( struct sockaddr *) & snl , sizeof ( struct sockaddr_nl));
+    if (retval < 0) {
+        printf ( "bind failed: %s" , strerror ( errno ));
+        close ( hotplug_sock );
+        hotplug_sock = -1;
+        return -1;
+    }
 
-	return hotplug_sock ;
+    return hotplug_sock ;
 }
 
 int connectsocket()
 {
 
-	struct sockaddr_in s_add,c_add;
-	int sin_size;
-	int cfd = socket(AF_INET, SOCK_STREAM, 0);
-	int portnum = 8699; 
-	if(-1 == cfd)
-	{
-		printf("socket fail ! \r\n");
-		return -1;
-	}
+    struct sockaddr_in s_add,c_add;
+    int sin_size;
+    int cfd = socket(AF_INET, SOCK_STREAM, 0);
+    int portnum = 8699; 
+    if(-1 == cfd)
+    {
+        printf("socket fail ! \r\n");
+        return -1;
+    }
 
-	bzero(&s_add,sizeof(struct sockaddr_in));
-	s_add.sin_family=AF_INET;
-	s_add.sin_addr.s_addr= inet_addr("127.0.0.1");
-	s_add.sin_port=htons(portnum);
+    bzero(&s_add,sizeof(struct sockaddr_in));
+    s_add.sin_family=AF_INET;
+    s_add.sin_addr.s_addr= inet_addr("127.0.0.1");
+    s_add.sin_port=htons(portnum);
 
-	if(-1 == connect(cfd,(struct sockaddr *)(&s_add), sizeof(struct sockaddr)))
-	{
-		printf("connect fail !\r\n");
-		return -1;
-	}
-	return cfd;
+    if(-1 == connect(cfd,(struct sockaddr *)(&s_add), sizeof(struct sockaddr)))
+    {
+        printf("connect fail !\r\n");
+        return -1;
+    }
+    return cfd;
 }
 
-int main(int argc, char **argv)
+static GtkWidget* entry1;
+char *arg_file;
+
+void on_button_clicked (GtkWidget* button,gpointer data)
 {
-	int cfd;
-	int recbytes;
-	char buffer[1024]={0};   
-	char cmd[1024]={0};
-	//int portnum = cal_num(argv[1]); 
+    /*
+     *const gchar *gtk_entry_get_text(GtkEntry *entry)
+     *     获得当前文本输入构件的内容
+     */
+    if ((int)data == 1)
+    {
+        printf("arg:%s\n",arg_file);
+        gtk_widget_destroy(window);
+        return;
+    }
+    if ((int)data == 2)
+    {
+        int cfd;
+        int recbytes;
+        char buffer[1024]={0};   
+        char cmd[1024]={0};
+        char send1[20]={0},send2[20]={0};
+        const gchar* vm_name = gtk_entry_get_text(GTK_ENTRY(entry1));
+        cfd = connectsocket();
+        strcat(send1,"send1 ");
+        strcat(send1,vm_name);
+        if(-1 == write(cfd,send1,strlen(send1)))
+        {
+            printf("write fail!\r\n");
+            gtk_widget_destroy(window);
+            return;
+        }
+        printf("send to client send1 success!\n");//wait device
+        recv (cfd , &buffer, sizeof(buffer), 0); 
+        printf("judge result:%s\n",buffer);
+        close(cfd);
 
-	cfd = connectsocket();
-	if(-1 == write(cfd,"send1 test2",11))
-	{
-		printf("write fail!\r\n");
-		return -1;
-	}
-	printf("send to client send1 success!\n");//wait device
-	recv (cfd , &buffer, sizeof(buffer), 0); 
-	printf("judge result:%s\n",buffer);
-	close(cfd);
+        if(strcmp(buffer,"NO") == 0)
+        {
+            printf("Now Close this Application.\n");
+            gtk_widget_destroy(window);
+            return;
+            printf("Closed!\n");
+        }
 
-	if(strstr(buffer,"NO"))
-		return 0;
+        int hotplug_sock  = init_hotplug_sock ();
+        while (1)
+        {
+            char buf [UEVENT_BUFFER_SIZE *2] = {0};
+            recv (hotplug_sock , &buf, sizeof (buf), 0); 
+            if(sizeof(buf)>0)
+                break;
+        }
+        sleep(5);
+        strcat(cmd,"cp ");
+        strcat(cmd,arg_file);
+        strcat(cmd," /media/EXCHANGE");
+        system(cmd);
+	sleep(3);
+        system("umount /media/EXCHANGE");
+        cfd = connectsocket();
+        if(cfd!=-1)
+        {
+            strcat(send2,"send2 ");
+            strcat(send2,vm_name);
+            if(-1 == write(cfd,send2,strlen(send2)))
+            {
+                printf("write fail!\r\n");
+                gtk_widget_destroy(window);
+                return;
+            }
+        }
+        else
+        {
+            printf("connect error!");
+            gtk_widget_destroy(window);
+            return;
+        }
+        close(cfd);
+        gtk_widget_destroy(window);
+        return;
+    }
 
-	int hotplug_sock  = init_hotplug_sock ();
-	while (1)
-	{
-		char buf [UEVENT_BUFFER_SIZE *2] = {0};
-		recv (hotplug_sock , &buf, sizeof (buf), 0); 
-		if(sizeof(buf)>0)
-			break;
-	}
-	sleep(5);
-	strcat(cmd,"cp ");
-	strcat(cmd,argv[1]);
-	strcat(cmd," /media/EXCHANGE");
-	system(cmd);
-	system("umount /media/EXCHANGE");
-	cfd = connectsocket();
-	if(cfd!=-1)
-	{
-		if(-1 == write(cfd,"send2 test2",11))
-		{
-			printf("write fail!\r\n");
-			return -1;
-		}
-		// if(recv(cfd,buffer,1024, 0)!=-1)
-		// {
-		//         printf("received responds! %s\n",buffer);
-		// } 
-	}
-	else
-	{
-		printf("connect error!");
-		return -1;
-	}
-	close(cfd);
-	return 0;
+
+}
+
+int main(int argc,char* argv[])
+{
+    GtkWidget* box;
+    GtkWidget* box1;
+    GtkWidget* box2;
+    GtkWidget* box3;
+    GtkWidget* label1;
+    GtkWidget* button;
+    GtkWidget* sep;
+    arg_file = argv[1];
+    //初始化
+    gtk_init(&argc,&argv);
+    //设置窗口
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    g_signal_connect(G_OBJECT(window),"destroy",G_CALLBACK(gtk_main_quit),NULL);
+    gtk_window_set_title(GTK_WINDOW(window),"Send To Other VM");
+    gtk_window_set_position(GTK_WINDOW(window),GTK_WIN_POS_CENTER);
+    gtk_container_set_border_width(GTK_CONTAINER(window),20);
+
+    box = gtk_vbox_new(FALSE,0);
+    gtk_container_add(GTK_CONTAINER(window),box);
+    box1 = gtk_hbox_new(FALSE,0);
+    gtk_box_pack_start(GTK_BOX(box),box1,FALSE,FALSE,5);
+    box2 = gtk_hbox_new(FALSE,0);
+    gtk_box_pack_start(GTK_BOX(box),box2,FALSE,FALSE,5);
+    sep = gtk_hseparator_new();//分割线
+    gtk_box_pack_start(GTK_BOX(box),sep,FALSE,FALSE,5);
+    box3 = gtk_hbox_new(FALSE,0);
+    gtk_box_pack_start(GTK_BOX(box),box3,TRUE,TRUE,5);
+
+
+    label1 = gtk_label_new("VM Name ：");
+    entry1 = gtk_entry_new();
+    gtk_box_pack_start(GTK_BOX(box1),label1,FALSE,FALSE,5);
+    gtk_box_pack_start(GTK_BOX(box1),entry1,FALSE,FALSE,5);
+
+    button = gtk_button_new_with_label("Send");
+    g_signal_connect(G_OBJECT(button),"clicked",G_CALLBACK(on_button_clicked),(gpointer)2);
+    gtk_box_pack_start(GTK_BOX(box3),button,TRUE,TRUE,10);
+    gtk_widget_show(button);
+
+    button = gtk_button_new_with_label("Cancel");
+    g_signal_connect(G_OBJECT(button),"clicked",G_CALLBACK(on_button_clicked),(gpointer)1);
+    // g_signal_connect_swapped(G_OBJECT(button),"clicked",G_CALLBACK(gtk_widget_destroy),window);
+    gtk_box_pack_start(GTK_BOX(box3),button,TRUE,TRUE,5);
+    gtk_widget_show(button);
+
+    gtk_widget_show_all(window);
+    gtk_main();
+    return FALSE;
 }
